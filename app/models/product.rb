53aacd7e1c8b74c99bc393960541fd72cfbc194d
Antonio Reuter <antonioreuter@gmail.com>
2014-08-11 22:37:38 -0300

    terceiro commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
 
+  scope :latest, -> { order(:updated_at).last }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -8,5 +13,16 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
 
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present!')
+      return false
+    end
+  end
 
 end
