a1cd64443fff4746ca7884986f9a7e4aecf0cc9b
Antonio Reuter <antonioreuter@gmail.com>
2014-08-15 00:22:14 -0300

    bug fixed

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,10 +2,9 @@ class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
   def add_product(product_id)
-    binding.pry
     current_item = line_items.find_by(product_id: product_id)
     if current_item
-      current_item.quantity = current_item.quantity.nil? ? 1 : current_item.quantity + 1
+      current_item.quantity += 1
     else
       current_item = line_items.build(product_id: product_id)
     end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,4 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
-
-  attribute quantity, default: 1
 end
